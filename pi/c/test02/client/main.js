821f1b54c60b89b581543a96ff72c5f8bcff19b5
takumus <me@takumus.com>
2016-06-16 19:43:03 +0900

    AIR使わない。やっぱりwebsocket.ioでやった。

---
+++ pi/c/test02/client/main.js
@@ -0,0 +1,36 @@
+window.addEventListener('load',function() {
+	var host = "ws://raspberrypi.local:3000/";
+	var socket = new WebSocket(host);
+
+	document.body.innerText = "connecting";
+	socket.addEventListener('open', function() {
+		document.body.innerText = "connected";
+	});
+	socket.addEventListener('message', function(message) {
+
+	});
+	socket.addEventListener('close', function() {
+
+	});
+	window.addEventListener('unload', function() {
+		socket.onclose();
+	});
+
+	document.addEventListener('mousemove', function(e) {
+		var y = e.clientY/window.innerHeight;
+		send(y);
+	});
+	document.addEventListener('touchmove', function(e) {
+		var y = e.touches[0].clientY/window.innerHeight;
+		send(y);
+	});
+
+	var send = function(y) {
+		var id = Math.floor(y * 16);
+		var str = "";
+		for(var i = 0; i < 16; i ++){
+			str += i==id?'1':'0';
+		}
+		socket.send(str+'\n');
+	}
+});
--- pi/c/test02/pi/server.js
+++ pi/c/test02/pi/server.js
@@ -12,13 +12,15 @@ app.on('close', (code) => {
 
 });
 
-const server = net.createServer((conn) => {
-	console.log('connected');
-	conn.on('data', (data) => {
+var ws = require('websocket.io');
+
+const server = ws.listen(3000, ()=> {
+	console.log('Server is running.');
+});
+
+server.on('connection', (socket) => {
+	socket.on('message', (data) => {
 		console.log(data.toString());
 		app.stdin.write(new Buffer(data + '\n'));
 	});
-	conn.on('close', () => {
-		console.log('disconnected');
-	});
-}).listen(3000);
+});
