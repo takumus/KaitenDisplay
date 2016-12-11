9857c504b26663319dd3cec541605fa71ad59e7f
takumus <me@takumus.com>
2016-12-11 16:17:15 +0900

    websocket -> socket -> localの流れが完成。

--- web_server/server.js
+++ web_server/server.js
@@ -1,20 +1,45 @@
-const PORT = 3001;
+"use strict"
+const SOCKET_PORT = 3001;
+const WS_PORT = 3002;
+
 const net = require('net');
+var ws = require("nodejs-websocket")
+
 const clients = {};
-const server = net.createServer((conn)=>{
-	console.log('server-> tcp server created');
-	conn.on('data', (data)=>{
-		console.log('server-> ' + data + ' from ' + conn.remoteAddress + ':' + conn.remotePort);
-		conn.write('server -> Repeating: ' + data);
+let primarySocket;
+
+let key = "";
+
+const server = net.createServer((socket)=>{
+	socket.on('data', (data)=>{
 	});
-	conn.on('close', ()=>{
-		console.log('server-> client closed connection');
+	socket.on('close', ()=>{
+		console.log('closed socket');
 	});
-	let s = "+";
-	for(let i = 0; i < 20; i ++){
-		s += "aaaaaaaaaa";
-	}
-	conn.write(s);
-	conn.write("-\n");
-}).listen(PORT);
-console.log('listening on port ' + PORT);
+	primarySocket = socket;
+}).listen(SOCKET_PORT);
+
+const ws_server = ws.createServer(function (conn) {
+    console.log("client connected")
+	console.log(ws_server.connections.length);
+    conn.on("text", function (str) {
+		try{
+			const data = JSON.parse(str);
+			if(data.key != key){
+				conn.close();
+				console.log("get data from old user");
+				return;
+			}
+			console.log("send to local server");
+			primarySocket.send(data.data);
+		}catch(e){
+			console.log(e.message);
+		}
+    })
+    conn.on("close", function (code, reason) {
+        console.log("client disconnected")
+    })
+}).listen(WS_PORT);
+
+console.log('socket listening on port ' + SOCKET_PORT);
+console.log('websocket listening on port ' + WS_PORT);
